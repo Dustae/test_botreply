848db7f4628caef13dde040076e484dc1011862f
dustae <dout.vgi@gmail.com>
2025-03-19 04:45:31 +0700

    Edit default amount

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ def home():
 def getdata_jbkk():
     try:
         origin_url = "https://jobbkk.com/%E0%B8%AB%E0%B8%B2%E0%B8%87%E0%B8%B2%E0%B8%99/%E0%B9%84%E0%B8%AD%E0%B8%97%E0%B8%B5,%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%9D%E0%B8%B6%E0%B8%81%E0%B8%87%E0%B8%B2%E0%B8%99"
-        data_amount = 4  # Default
+        data_amount = 3  # Default
         url = origin_url
         response = requests.get(url)
 
